5b42f2fd7ece7a76eb0b84375faf4fbb3262409c
Kenta_Nakajima <knknkn1162@gmail.com>
2022-10-08 19:29:40 +0900

    add log in thread per message pattern

--- thread_per_message/main.go
+++ thread_per_message/main.go
@@ -7,10 +7,6 @@ import (
     "time"
 )
 
-type Data struct {
-    Message string
-    Number int
-}
 
 func do_calc(num int) {
     rand.Seed(time.Now().UnixNano())
@@ -18,16 +14,20 @@ func do_calc(num int) {
     time.Sleep(time.Duration(m) * time.Millisecond)
 }
 
-func request(num int) <-chan Data {
-    res := make(chan Data)
+func request(num int) <-chan bool {
+    res := make(chan bool)
     var wg sync.WaitGroup
     for i := 0; i < num; i++ {
         wg.Add(1)
+        fmt.Printf("req #%v begin\n", i)
         go func(i int) {
             defer wg.Done()
+            // Assume that return value is not necessary
+            fmt.Printf("goroutine #%v begin\n", i)
             do_calc(1000)
-            fmt.Printf("request %v %v\n", string(i+0x41), i)
+            fmt.Printf("goroutine #%v -> %v\n", string(i+0x41), i)
         }(i)
+        fmt.Printf("req #%v end\n", i)
     }
     go func() {
         defer close(res)
